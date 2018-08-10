f3a6865d1f2948b57a3cd8858ccc0fcb3c5ce145
PRINCE HECTOR <nabodip064@gmail.com>
2018-08-10 13:14:40 +0600

    added

---
+++ resources/views/maxim/booking_list/booking_View_Details.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+@section('page_heading', trans("others.mxp_menu_booking_view_details") )
+@section('section')
+    Booking View Details and booking no is {{ $booking_id }}
+@endsection
+
+
